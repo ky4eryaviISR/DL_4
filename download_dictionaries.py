e690fae7c6ef7d58750b2718aa62449a167afb07
VovaPsevkin <psevkin@gmail.com>
2020-01-15 22:52:57 +0200

    Update Project

--- download_dictionaries.py
+++ download_dictionaries.py
@@ -2,13 +2,18 @@ import os
 from tqdm import tqdm
 import numpy as np
 
-
 if not os.path.exists('glove'):
-  os.system(f'wget http://nlp.stanford.edu/data/glove.6B.zip && '
-            f'unzip glove.6B.zip && '
-            f'mkdir glove && '
-            f'mv glove*.txt')
-# if not os.path.exists(root+'fasttext'):
-#   !wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/wiki-news-300d-1M.vec.zip && unzip wiki-news-300d-1M.vec.zip && unzip '/content/'wiki-news-300d-1M.vec.zip && mkdir '/content/drive/My Drive/DL_4/'fasttext && mv '/content/'wiki-news-300d-1M.vec '/content/drive/My Drive/DL_4/'fasttext
-# if not os.path.exists(root+'word2vec'):
-#   !wget "https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz" && gunzip /content/GoogleNews-vectors-negative300.bin.gz &&  mkdir '/content/drive/My Drive/DL_4/'word2vec && mv /content/GoogleNews-vectors-negative300.bin '/content/drive/My Drive/DL_4/'word2vec
+    os.system(f'wget http://nlp.stanford.edu/data/glove.6B.zip && '
+              f'unzip glove.6B.zip && '
+              f'mkdir glove && '
+              f'mv glove*.txt')
+if not os.path.exists('fasttext'):
+  os.system(f'wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/wiki-news-300d-1M.vec.zip &&'
+            f'unzip wiki-news-300d-1M.vec.zip && '
+            f'mkdir fasttext && '
+            f'mv wiki-news-300d-1M.vec fasttext')
+if not os.path.exists('word2vec'):
+  os.system(f'wget https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz && '
+            f'gunzip GoogleNews-vectors-negative300.bin.gz && '
+            f' mkdir word2vec && '
+            f'mv GoogleNews-vectors-negative300.bin word2vec')
