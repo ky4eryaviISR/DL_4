3eb5cf3e00e9c82766115e574d2b9031851c0d33
Vladimir <Vovchik.balagula@gmail.com>
2020-01-15 23:00:51 +0200

    add train and evaluate
    Signed-off-by: Vladimir <Vovchik.balagula@gmail.com>

--- main_file.py
+++ main_file.py
@@ -1,20 +1,50 @@
-import io
-
+from torch.nn import CrossEntropyLoss
+from torch.optim import Adam
+from torch.functional import F
+from tqdm import tqdm
+from Model import MainModel
 from dataloader import SNLI_DataLoader
 
 
-# def load_vectors(fname):
-#     fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
-#     n, d = map(int, fin.readline().split())
-#     data = {}
-#     for line in fin:
-#         tokens = line.rstrip().split(' ')
-#         data[tokens[0]] = map(float, tokens[1:])
-#     return data
-#
-# load_vectors('data/fasttext300d-2m/crawl-300d-2M.vec')
+def evaluate(model, data, criterion, set_name):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for x, y in data:
+        output = model(x)
+        test_loss += criterion(output, y).item()
+        pred = output.max(1, keepdim=True)[1]
+        correct += pred.eq(y.view_as(pred)).sum()
+    test_loss /= len(data.dataset)
+    test_acc = 100. * correct / len(data.dataset)
+    print('{} set: Average loss: {:.4f}, Accuracy: {}/{}({:.0f}%)'.
+          format(set_name, test_loss, correct, len(data.dataset), test_acc))
+    return test_acc, test_loss
 
 
-if __name__=='__main__':
+def train(model, tr_data, val_data, opt, epoch=10):
+    criterion = CrossEntropyLoss()
+    for i in range(epoch):
+        model.train()
+        for x, y in tqdm(tr_data):
+            opt.zero_grad()
+            preds = model(x)
+            loss = criterion(preds, y)
+            loss.backward()
+            opt.step()
+
+        evaluate(model, tr_data, criterion, 'Train')
+        evaluate(model, val_data, criterion, 'Validation')
+
+
+def main():
+    main()
+    model = MainModel(None, None)
+    opt = Adam(model.parameters(), lr=1e-2)
     dt = SNLI_DataLoader()
+    train(model, dt.train_iter, dt.val_iter, opt)
     print('x')
+
+
+if __name__ == '__main__':
+    main()
