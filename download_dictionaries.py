153c2e75b713e5a1534a2c0c3204ac7eb98001c9
VovaPsevkin <psevkin@gmail.com>
2020-02-01 02:49:52 +0200

    Upgrade Code doenload_dictionaries.py

--- download_dictionaries.py
+++ download_dictionaries.py
@@ -4,16 +4,18 @@ if not os.path.exists('data/glove'):
     os.system(f'wget http://nlp.stanford.edu/data/glove.6B.zip && '
               f'unzip glove.6B.zip && '
               f'mkdir data/glove && '
-              f'mv glove*.txt data/glove')
+              f'mv glove.6B.300d.txt data/glove && '
+              f'rm -rf glove*.txt && rm -rf glove.6B.zip')
 
 if not os.path.exists('data/fasttext'):
   os.system(f'wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M.vec.zip &&'
             f'unzip crawl-300d-2M.vec.zip && '
             f'mkdir data/fasttext && '
-            f'mv data/crawl-300d-2M.vec data/fasttext')
+            f'mv crawl-300d-2M.vec data/fasttext && '
+            f'rm -rf crawl-300d-2M.vec.zip')
 
-if not os.path.exists('data/word2vec'):
-  os.system(f'wget https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz && '
-            f'gunzip GoogleNews-vectors-negative300.bin.gz && '
-            f' mkdir data/word2vec && '
-            f'mv GoogleNews-vectors-negative300.bin data/word2vec')
+if not os.path.exists('data/levy'):
+    os.system(f'wget http://u.cs.biu.ac.il/~yogo/data/syntemb/bow2.words.bz2 && '
+              f'bzip2 -d bow2.words.bz2 && '
+              f'mkdir data/levy && '
+              f'mv bow2.words data/levy')
